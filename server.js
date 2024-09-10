4175e80fa63b81f8d6424ff13f7624a6e9e41109
jborruel308@me.com <jborruel308@me.com>
2024-09-09 18:59:57 -0700

    fruit models

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express = require("express");
+require('dotenv').config()
 
+const express = require("express");
+const mongoose = require('mongoose');
 const app = express ();
 
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
 
+const Fruit = require('./models/fruit.js');
 
+app.get("/", async (req,res) => {
+    res.render('index.ejs');
+});
 
+app.get("/fruits/new", (req, res)=> {
+    res.render("fruits/new.ejs");
+});
 
 
 
