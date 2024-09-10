bd9505459758d2a105dca952cb7e28b6dd3ca68b
jborruel308@me.com <jborruel308@me.com>
2024-09-09 20:12:05 -0700

    add show, index. and new pages for fruits

--- server.js
+++ server.js
@@ -1,25 +1,63 @@
+//===============inputs================//
 require('dotenv').config()
 
 const express = require("express");
 const mongoose = require('mongoose');
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
 const app = express ();
+
+//===============Mongoose================//
 
 mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`)
 })
-
 const Fruit = require('./models/fruit.js');
 
+
+//===============Middleware================//
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+
+
+//===============Routes================//
 app.get("/", async (req,res) => {
     res.render('index.ejs');
 });
+
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  console.log(allFruits);
+  res.render('fruits/index.ejs', {fruits: allFruits})
+});
+
 
 app.get("/fruits/new", (req, res)=> {
     res.render("fruits/new.ejs");
 });
 
+app.get("/fruits/:fruitId", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/show.ejs", {fruit: foundFruit});
+  });
+  
 
+
+app.post("/fruits", async (req, res) => {
+    // console.log(req.body);
+    if (req.body.isReadyToEat === 'on'){
+        req.body.isReadyToEat = true
+    }else {
+        req.body.isReadyToEat = false
+    }
+    await Fruit.create(req.body);
+    // console.log(req.body)
+
+    res.redirect("/fruits")
+});
 
 
 app.listen(3000, () => {
